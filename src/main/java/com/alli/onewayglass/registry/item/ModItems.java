612b6d507c5b3156f92ea632fafde1ac2728da0d
technoalli3 <92867135+technoalli3@users.noreply.github.com>
2022-02-28 23:34:06 -0500

    Upload

--- src/main/java/com/alli/onewayglass/registry/item/ModItems.java
+++ src/main/java/com/alli/onewayglass/registry/item/ModItems.java
@@ -2,6 +2,7 @@ package com.alli.onewayglass.registry.item;
 
 import com.alli.onewayglass.OneWayGlass;
 import com.alli.onewayglass.registry.block.ModBlocks;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
@@ -11,7 +12,7 @@ import net.minecraft.util.registry.Registry;
 public class ModItems {
 
     //Items
-
+    public static final Item GLASS_FRAME = new Item(new FabricItemSettings().group(ItemGroup.MATERIALS));
     //Block Items
     public static final BlockItem BLACK_SECURITY_GLASS = new BlockItem(ModBlocks.BLACK_SECURITY_GLASS, new Item.Settings().group(ItemGroup.DECORATIONS));
     public static final BlockItem WHITE_SECURITY_GLASS = new BlockItem(ModBlocks.WHITE_SECURITY_GLASS, new Item.Settings().group(ItemGroup.DECORATIONS));
@@ -48,7 +49,7 @@ public class ModItems {
 
     public static void registerItems() {
         //Items
-
+        Registry.register(Registry.ITEM, new Identifier(OneWayGlass.MOD_ID, "glass_frame"), GLASS_FRAME);
         /*********************************/
 
         //Block Items
