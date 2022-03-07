b54afee8b85fedd48d354a292d71e8978855e371
technoalli3 <92867135+technoalli3@users.noreply.github.com>
2022-03-07 18:04:19 -0500

    Glass tools

--- src/main/java/com/alli/onewayglass/registry/tool/CustomPickaxe.java
+++ src/main/java/com/alli/onewayglass/registry/tool/CustomPickaxe.java
@@ -1,10 +1,40 @@
 package com.alli.onewayglass.registry.tool;
 
+import net.minecraft.block.BlockState;
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.command.argument.ItemStackArgument;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.PickaxeItem;
 import net.minecraft.item.ToolMaterial;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.stat.Stats;
+import net.minecraft.text.Text;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+import java.awt.*;
 
 public class CustomPickaxe extends PickaxeItem {
     public CustomPickaxe(ToolMaterial material, int attackDamage, float attackSpeed, Settings settings) {
         super(material, attackDamage, attackSpeed, settings);
     }
+
+    @Override
+    public boolean postHit(ItemStack stack, LivingEntity target, LivingEntity attacker) {
+        NbtCompound nbt = stack.getOrCreateNbt();
+        int fragments = nbt.getInt("fragment");
+        nbt.putInt("fragment",fragments+1);
+        return super.postHit(stack, target, attacker);
+    }
+
+    @Override
+    public boolean postMine(ItemStack stack, World world, BlockState state, BlockPos pos, LivingEntity miner) {
+        return super.postMine(stack, world, state, pos, miner);
+    }
+
+    
+
 }
