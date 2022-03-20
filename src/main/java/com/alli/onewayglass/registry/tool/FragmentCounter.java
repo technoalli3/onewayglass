aef4fcfb4c842c0e02986263a69fb94298e4a14d
technoalli3 <92867135+technoalli3@users.noreply.github.com>
2022-03-20 10:07:39 -0400

    Glass tool icon rendering

---
+++ src/main/java/com/alli/onewayglass/registry/tool/FragmentCounter.java
@@ -0,0 +1,9 @@
+package com.alli.onewayglass.registry.tool;
+
+import net.minecraft.item.Item;
+
+public class FragmentCounter extends Item {
+    public FragmentCounter(Settings settings) {
+        super(settings);
+    }
+}
