68a149ebadf780d40b8e1d2f0db07c37ad518d64
technoalli3 <92867135+technoalli3@users.noreply.github.com>
2022-12-25 12:02:45 -0500

    fixed directory names, added atlas file to load custom assets, and removed item groups (temporarily).

--- src/main/java/com/alli/onewayglass/registry/item/ModItems.java
+++ src/main/java/com/alli/onewayglass/registry/item/ModItems.java
@@ -4,8 +4,9 @@ import com.alli.onewayglass.OneWayGlass;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 
 import static com.alli.onewayglass.registry.tool.GlassToolItem.*;
 import static net.minecraft.item.Items.BUCKET;
@@ -13,48 +14,48 @@ import static net.minecraft.item.Items.BUCKET;
 public class ModItems {
 
     //Items
-    public static final Item GLASS_FRAME = new Item(new FabricItemSettings().group(ItemGroup.MATERIALS));
+    public static final Item GLASS_FRAME = new Item(new FabricItemSettings());
     //Prepped molds
-    public static final Item PREPPED_AXE_MOLD = new Item(new FabricItemSettings().group(ItemGroup.MATERIALS));
-    public static final Item PREPPED_PICKAXE_MOLD = new Item(new FabricItemSettings().group(ItemGroup.MATERIALS));
-    public static final Item PREPPED_HOE_MOLD = new Item(new FabricItemSettings().group(ItemGroup.MATERIALS));
-    public static final Item PREPPED_SWORD_MOLD = new Item(new FabricItemSettings().group(ItemGroup.MATERIALS));
-    public static final Item PREPPED_SHOVEL_MOLD = new Item(new FabricItemSettings().group(ItemGroup.MATERIALS));
+    public static final Item PREPPED_AXE_MOLD = new Item(new FabricItemSettings());
+    public static final Item PREPPED_PICKAXE_MOLD = new Item(new FabricItemSettings());
+    public static final Item PREPPED_HOE_MOLD = new Item(new FabricItemSettings());
+    public static final Item PREPPED_SWORD_MOLD = new Item(new FabricItemSettings());
+    public static final Item PREPPED_SHOVEL_MOLD = new Item(new FabricItemSettings());
     //Finished molds
-    public static final Item FINISHED_AXE_MOLD = new Item(new FabricItemSettings().group(ItemGroup.MATERIALS).recipeRemainder(GLASS_AXE));
-    public static final Item FINISHED_PICKAXE_MOLD = new Item(new FabricItemSettings().group(ItemGroup.MATERIALS).recipeRemainder(GLASS_PICKAXE));
-    public static final Item FINISHED_HOE_MOLD = new Item(new FabricItemSettings().group(ItemGroup.MATERIALS).recipeRemainder(GLASS_HOE));
-    public static final Item FINISHED_SWORD_MOLD = new Item(new FabricItemSettings().group(ItemGroup.MATERIALS).recipeRemainder(GLASS_SWORD));
-    public static final Item FINISHED_SHOVEL_MOLD = new Item(new FabricItemSettings().group(ItemGroup.MATERIALS).recipeRemainder(GLASS_SHOVEL));
+    public static final Item FINISHED_AXE_MOLD = new Item(new FabricItemSettings().recipeRemainder(GLASS_AXE));
+    public static final Item FINISHED_PICKAXE_MOLD = new Item(new FabricItemSettings().recipeRemainder(GLASS_PICKAXE));
+    public static final Item FINISHED_HOE_MOLD = new Item(new FabricItemSettings().recipeRemainder(GLASS_HOE));
+    public static final Item FINISHED_SWORD_MOLD = new Item(new FabricItemSettings().recipeRemainder(GLASS_SWORD));
+    public static final Item FINISHED_SHOVEL_MOLD = new Item(new FabricItemSettings().recipeRemainder(GLASS_SHOVEL));
     //Buckets
-    public static final Item SAND_BUCKET = new Item(new FabricItemSettings().group(ItemGroup.MATERIALS));
-    public static final Item POWDERED_GLASS_BUCKET = new Item(new FabricItemSettings().group(ItemGroup.MATERIALS));
-    public static final Item MOLTEN_GLASS_BUCKET = new Item(new FabricItemSettings().group(ItemGroup.MATERIALS).recipeRemainder(BUCKET));
+    public static final Item SAND_BUCKET = new Item(new FabricItemSettings());
+    public static final Item POWDERED_GLASS_BUCKET = new Item(new FabricItemSettings());
+    public static final Item MOLTEN_GLASS_BUCKET = new Item(new FabricItemSettings().recipeRemainder(BUCKET));
     //powdered glass
-    public static final Item POWDERED_GLASS = new Item(new FabricItemSettings().group(ItemGroup.MATERIALS).maxCount(2));
+    public static final Item POWDERED_GLASS = new Item(new FabricItemSettings().maxCount(2));
 
 
     public static void registerItems() {
         //Items
-        Registry.register(Registry.ITEM, new Identifier(OneWayGlass.MOD_ID, "glass_frame"), GLASS_FRAME);
+        Registry.register(Registries.ITEM, new Identifier(OneWayGlass.MOD_ID, "glass_frame"), GLASS_FRAME);
         //Prepped molds
-        Registry.register(Registry.ITEM, new Identifier(OneWayGlass.MOD_ID, "prepped_axe_mold"), PREPPED_AXE_MOLD);
-        Registry.register(Registry.ITEM, new Identifier(OneWayGlass.MOD_ID, "prepped_pickaxe_mold"), PREPPED_PICKAXE_MOLD);
-        Registry.register(Registry.ITEM, new Identifier(OneWayGlass.MOD_ID, "prepped_shovel_mold"), PREPPED_SHOVEL_MOLD);
-        Registry.register(Registry.ITEM, new Identifier(OneWayGlass.MOD_ID, "prepped_sword_mold"), PREPPED_SWORD_MOLD);
-        Registry.register(Registry.ITEM, new Identifier(OneWayGlass.MOD_ID, "prepped_hoe_mold"), PREPPED_HOE_MOLD);
+        Registry.register(Registries.ITEM, new Identifier(OneWayGlass.MOD_ID, "prepped_axe_mold"), PREPPED_AXE_MOLD);
+        Registry.register(Registries.ITEM, new Identifier(OneWayGlass.MOD_ID, "prepped_pickaxe_mold"), PREPPED_PICKAXE_MOLD);
+        Registry.register(Registries.ITEM, new Identifier(OneWayGlass.MOD_ID, "prepped_shovel_mold"), PREPPED_SHOVEL_MOLD);
+        Registry.register(Registries.ITEM, new Identifier(OneWayGlass.MOD_ID, "prepped_sword_mold"), PREPPED_SWORD_MOLD);
+        Registry.register(Registries.ITEM, new Identifier(OneWayGlass.MOD_ID, "prepped_hoe_mold"), PREPPED_HOE_MOLD);
         //Finished molds
-        Registry.register(Registry.ITEM, new Identifier(OneWayGlass.MOD_ID, "finished_axe_mold"), FINISHED_AXE_MOLD);
-        Registry.register(Registry.ITEM, new Identifier(OneWayGlass.MOD_ID, "finished_pickaxe_mold"), FINISHED_PICKAXE_MOLD);
-        Registry.register(Registry.ITEM, new Identifier(OneWayGlass.MOD_ID, "finished_shovel_mold"), FINISHED_SHOVEL_MOLD);
-        Registry.register(Registry.ITEM, new Identifier(OneWayGlass.MOD_ID, "finished_sword_mold"), FINISHED_SWORD_MOLD);
-        Registry.register(Registry.ITEM, new Identifier(OneWayGlass.MOD_ID, "finished_hoe_mold"), FINISHED_HOE_MOLD);
+        Registry.register(Registries.ITEM, new Identifier(OneWayGlass.MOD_ID, "finished_axe_mold"), FINISHED_AXE_MOLD);
+        Registry.register(Registries.ITEM, new Identifier(OneWayGlass.MOD_ID, "finished_pickaxe_mold"), FINISHED_PICKAXE_MOLD);
+        Registry.register(Registries.ITEM, new Identifier(OneWayGlass.MOD_ID, "finished_shovel_mold"), FINISHED_SHOVEL_MOLD);
+        Registry.register(Registries.ITEM, new Identifier(OneWayGlass.MOD_ID, "finished_sword_mold"), FINISHED_SWORD_MOLD);
+        Registry.register(Registries.ITEM, new Identifier(OneWayGlass.MOD_ID, "finished_hoe_mold"), FINISHED_HOE_MOLD);
         //Buckets
-        Registry.register(Registry.ITEM, new Identifier(OneWayGlass.MOD_ID, "sand_bucket"), SAND_BUCKET);
-        Registry.register(Registry.ITEM, new Identifier(OneWayGlass.MOD_ID, "powdered_glass_bucket"), POWDERED_GLASS_BUCKET);
-        Registry.register(Registry.ITEM, new Identifier(OneWayGlass.MOD_ID, "molten_glass_bucket"), MOLTEN_GLASS_BUCKET);
+        Registry.register(Registries.ITEM, new Identifier(OneWayGlass.MOD_ID, "sand_bucket"), SAND_BUCKET);
+        Registry.register(Registries.ITEM, new Identifier(OneWayGlass.MOD_ID, "powdered_glass_bucket"), POWDERED_GLASS_BUCKET);
+        Registry.register(Registries.ITEM, new Identifier(OneWayGlass.MOD_ID, "molten_glass_bucket"), MOLTEN_GLASS_BUCKET);
         //powdered glass
-        Registry.register(Registry.ITEM, new Identifier(OneWayGlass.MOD_ID, "powdered_glass"), POWDERED_GLASS);
+        Registry.register(Registries.ITEM, new Identifier(OneWayGlass.MOD_ID, "powdered_glass"), POWDERED_GLASS);
 
     }
 }
--- src/main/java/com/alli/onewayglass/registry/tool/GlassToolItem.java
+++ src/main/java/com/alli/onewayglass/registry/tool/GlassToolItem.java
@@ -2,22 +2,23 @@ package com.alli.onewayglass.registry.tool;
 
 import com.alli.onewayglass.OneWayGlass;
 import net.minecraft.item.*;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 
 public class GlassToolItem {
-    public static ToolItem GLASS_SHOVEL = new ShovelItem(GlassToolMaterial.INSTANCE, 1.5F, -3.0F, new Item.Settings().group(ItemGroup.TOOLS));
-    public static ToolItem GLASS_SWORD = new SwordItem(GlassToolMaterial.INSTANCE, 3, -3.0F, new Item.Settings().group(ItemGroup.TOOLS));
-    public static ToolItem GLASS_PICKAXE = new CustomPickaxe(GlassToolMaterial.INSTANCE, 1, -2.8F, new Item.Settings().group(ItemGroup.TOOLS));
-    public static ToolItem GLASS_AXE = new CustomAxe(GlassToolMaterial.INSTANCE, 7.0F, -3.2F, new Item.Settings().group(ItemGroup.TOOLS));
-    public static ToolItem GLASS_HOE = new CustomHoe(GlassToolMaterial.INSTANCE, 7, -3.2F, new Item.Settings().group(ItemGroup.TOOLS));
+    public static ToolItem GLASS_SHOVEL = new ShovelItem(GlassToolMaterial.INSTANCE, 1.5F, -3.0F, new Item.Settings());
+    public static ToolItem GLASS_SWORD = new SwordItem(GlassToolMaterial.INSTANCE, 3, -3.0F, new Item.Settings());
+    public static ToolItem GLASS_PICKAXE = new CustomPickaxe(GlassToolMaterial.INSTANCE, 1, -2.8F, new Item.Settings());
+    public static ToolItem GLASS_AXE = new CustomAxe(GlassToolMaterial.INSTANCE, 7.0F, -3.2F, new Item.Settings());
+    public static ToolItem GLASS_HOE = new CustomHoe(GlassToolMaterial.INSTANCE, 7, -3.2F, new Item.Settings());
 
 
     public static void registerTools() {
-        Registry.register(Registry.ITEM, new Identifier(OneWayGlass.MOD_ID, "glass_shovel"), GLASS_SHOVEL);
-        Registry.register(Registry.ITEM, new Identifier(OneWayGlass.MOD_ID, "glass_sword"), GLASS_SWORD);
-        Registry.register(Registry.ITEM, new Identifier(OneWayGlass.MOD_ID, "glass_pickaxe"), GLASS_PICKAXE);
-        Registry.register(Registry.ITEM, new Identifier(OneWayGlass.MOD_ID, "glass_axe"), GLASS_AXE);
-        Registry.register(Registry.ITEM, new Identifier(OneWayGlass.MOD_ID, "glass_hoe"), GLASS_HOE);
+        Registry.register(Registries.ITEM, new Identifier(OneWayGlass.MOD_ID, "glass_shovel"), GLASS_SHOVEL);
+        Registry.register(Registries.ITEM, new Identifier(OneWayGlass.MOD_ID, "glass_sword"), GLASS_SWORD);
+        Registry.register(Registries.ITEM, new Identifier(OneWayGlass.MOD_ID, "glass_pickaxe"), GLASS_PICKAXE);
+        Registry.register(Registries.ITEM, new Identifier(OneWayGlass.MOD_ID, "glass_axe"), GLASS_AXE);
+        Registry.register(Registries.ITEM, new Identifier(OneWayGlass.MOD_ID, "glass_hoe"), GLASS_HOE);
     }
 }
